4296e37d7ff0ba7cb7d8cc87840edba3b3045273
zhangfenglu <58172121+Zhangfenglu71@users.noreply.github.com>
2026-01-05 22:10:14 +0800

    Add video spectrogram preprocessing pipeline

---
+++ datasets/__init__.py
@@ -0,0 +1,9 @@
+from .real_video_radar import RealVideoRadarConfig, RealVideoRadarDataset
+from .video_preprocess import VideoPreprocessConfig, VideoSpectrogramPreprocessor
+
+__all__ = [
+    "RealVideoRadarConfig",
+    "RealVideoRadarDataset",
+    "VideoPreprocessConfig",
+    "VideoSpectrogramPreprocessor",
+]
